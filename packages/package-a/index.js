c5ea1c8d577bd35ecb586ad82d16b54499893be7
Malash <i@malash.me>
2018-11-27 15:03:00 +0800

    disable preserveSymlinks

--- packages/package-a/index.js
+++ packages/package-a/index.js
@@ -4,6 +4,6 @@ const resolve = require('resolve');
 
 const basedir = './node_modules/@my-scope/package-b';
 
-console.log(resolve.sync('jquery', { basedir }));
-console.log(resolve.sync('../../node_modules/jquery', { basedir }));
+console.log(resolve.sync('jquery', { basedir, preserveSymlinks: false }));
+console.log(resolve.sync('../../node_modules/jquery', { basedir, preserveSymlinks: false }));
 
